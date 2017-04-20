4e28f760d33b991b7ea92ff37369bfb71bf017b7
MinSikMoon <msmoon.business@gmail.com>
2017-04-20 16:14:16 +0900

    스트링을 아스키코드, 즉 번호로 나타내기

---
+++ basic/string/Ascii11654.java
@@ -0,0 +1,13 @@
+package basic.string;
+import java.util.Scanner;
+
+public class Ascii11654 {
+	
+	
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		String str = in.next();
+		char c = str.charAt(0);
+		System.out.print((int)c);
+	}
+}
